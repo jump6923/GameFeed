3aaa59b585db35c8640bab71bf4d5c674077863a
jump6923 <jump6923@gmail.com>
2023-11-21 17:45:40 +0900

    first commit

---
+++ src/test/java/com/sparta/gamefeed/GameFeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.gamefeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameFeedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
