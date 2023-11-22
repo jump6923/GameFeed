ad2559594e068e7fafce439e7af2cf8ae292aa65
hwang <ghkdrbwjd369@naver.com>
2023-11-22 18:06:58 +0900

    Feat : Jwt Login feature

--- src/main/java/com/sparta/gamefeed/security/JwtAuthorizationFilter.java
+++ src/main/java/com/sparta/gamefeed/security/JwtAuthorizationFilter.java
@@ -1,4 +1,75 @@
 package com.sparta.gamefeed.security;
 
-public class JwtAuthorizationFilter {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sparta.gamefeed.dto.StatusResponseDto;
+import com.sparta.gamefeed.jwt.JwtUtil;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Slf4j(topic = "JWT 검증 및 인가")
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+    private final UserDetailsServiceImpl userDetailsService;
+
+    ObjectMapper ob = new ObjectMapper();
+
+    public JwtAuthorizationFilter(JwtUtil jwtUtil, UserDetailsServiceImpl userDetailsService) {
+        this.jwtUtil = jwtUtil;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String tokenValue = jwtUtil.getJwtFromHeader(request);
+
+        if(StringUtils.hasText(tokenValue)){
+            if(!jwtUtil.validateToken(tokenValue)){
+                String message = "토큰이 유효하지 않습니다.";
+                response.setStatus(400);
+                String json = ob.writeValueAsString(new StatusResponseDto(message, response.getStatus()));
+                PrintWriter writer = response.getWriter();
+                writer.println(json);
+                return;
+            }
+
+            Claims info = jwtUtil.getUserInforFromToken(tokenValue);
+
+            try {
+                setAuthentication(info.getSubject());
+            } catch (Exception e){
+                log.error(e.getMessage());
+                return;
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private void setAuthentication(String userId){
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        Authentication authentication = createAuthentication(userId);
+        context.setAuthentication(authentication);
+
+        SecurityContextHolder.setContext(context);
+    }
+
+    private Authentication createAuthentication(String userId){
+        UserDetails userDetails = userDetailsService.loadUserByUsername(userId);
+        return new UsernamePasswordAuthenticationToken(userDetails, null, null);
+    }
 }
---
+++ src/main/java/com/sparta/gamefeed/security/UserDetailsImpl.java
@@ -0,0 +1,56 @@
+package com.sparta.gamefeed.security;
+
+import com.sparta.gamefeed.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class UserDetailsImpl implements UserDetails {
+
+    private final User user;
+
+    public UserDetailsImpl(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUserId();
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
