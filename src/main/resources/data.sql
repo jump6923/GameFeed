b35ceaab89b5275e9bc111967a0fa9f19ba8135a
hwang <ghkdrbwjd369@naver.com>
2023-11-23 12:53:35 +0900

    Feat : Category / Folder / CategoryFolder Entity & default data insert

---
+++ src/main/resources/data.sql
@@ -0,0 +1,21 @@
+insert into category(categorytitle) values("LOL");
+insert into category(categorytitle) values("Lost Ark");
+insert into category(categorytitle) values("MapleStroy");
+insert into category(categorytitle) values("원신");
+
+insert into folder(foldertitle) values("공지사항");
+insert into folder(foldertitle) values("이벤트");
+insert into folder(foldertitle) values("팁게시판");
+
+insert into categoryfolder values(1,1,1);
+insert into categoryfolder values(2,1,2);
+insert into categoryfolder values(3,1,3);
+insert into categoryfolder values(4,2,1);
+insert into categoryfolder values(5,2,2);
+insert into categoryfolder values(6,2,3);
+insert into categoryfolder values(7,3,1);
+insert into categoryfolder values(8,3,2);
+insert into categoryfolder values(9,3,3);
+insert into categoryfolder values(10,4,1);
+insert into categoryfolder values(11,4,2);
+insert into categoryfolder values(12,4,3);
