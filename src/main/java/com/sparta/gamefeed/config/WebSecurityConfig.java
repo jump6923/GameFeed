d24a13bd036c1208399acc803df0327d97fbc80d
hwang <ghkdrbwjd369@naver.com>
2023-11-23 19:15:02 +0900

    Feat : Email 인증 및 Email 인증에 따른 로그인 성공 / 실패

--- src/main/java/com/sparta/gamefeed/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/gamefeed/config/WebSecurityConfig.java
@@ -64,7 +64,7 @@ public class WebSecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/api/user/**").permitAll()
                         .requestMatchers("/api/main/**").permitAll()
                         .requestMatchers("/api/email/**").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/api/post/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/posts/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/comments/**").permitAll()
                         .anyRequest().authenticated()
         );
