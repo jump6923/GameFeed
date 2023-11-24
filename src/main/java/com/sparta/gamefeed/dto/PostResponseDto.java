e9fe73b810a40b06c497488b1803ab7b3b2644f6
seastamp <seastamp10@gmail.com>
2023-11-24 16:46:26 +0900

    feat : 게시글 댓글 ResponseDto 수정

--- src/main/java/com/sparta/gamefeed/dto/PostResponseDto.java
+++ src/main/java/com/sparta/gamefeed/dto/PostResponseDto.java
@@ -14,6 +14,7 @@ public class PostResponseDto {
     private Long id;
     private String title;
     private String contents;
+    private int likeCount;
     private LocalDateTime createdAt;
     private LocalDateTime modifiedAt;
 
@@ -21,6 +22,7 @@ public class PostResponseDto {
         this.id = post.getId();
         this.title = post.getTitle();
         this.contents = post.getContents();
+        this.likeCount = post.getLikeCount();
         this.createdAt = post.getCreatedAt();
         this.modifiedAt = post.getModifiedAt();
     }
